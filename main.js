eae8c84c956da81379314993b5f6ea730cdf5a04
TA2k <tombox2020@gmail.com>
2023-02-17 19:45:08 +0100

    fix sleep check

--- main.js
+++ main.js
@@ -659,7 +659,7 @@ class Teslamotors extends utils.Adapter {
       const curState = await this.getStateAsync(vin + stateId);
       this.log.debug("Check state: " + vin + stateId);
       //laste update not older than 30min and last change not older then 30min
-      if (curState && (curState.ts <= Date.now() - 1800000 || curState.ts - curState.lc <= 1800000)) {
+      if (curState && (curState.ts >= Date.now() - 1800000 || curState.ts - curState.lc <= 1800000)) {
         this.log.debug(
           `Skip sleep waiting because state ${vin + stateId} changed in last 30min TS: ${new Date(curState.ts).toLocaleString()} LC: ${new Date(
             curState.lc
