d3bb98f810611d95ca03389554e7ab34906e87c6
TA2k <tombox2020@gmail.com>
2023-02-18 23:37:59 +0100

    revert change

--- main.js
+++ main.js
@@ -664,7 +664,7 @@ class Teslamotors extends utils.Adapter {
       }
 
       //laste update not older than 30min and last change not older then 30min
-      if (curState && (curState.ts >= Date.now() - 1800000 || curState.ts - curState.lc <= 1800000)) {
+      if (curState && (curState.ts <= Date.now() - 1800000 || curState.ts - curState.lc <= 1800000)) {
         this.log.debug(
           `Skip sleep waiting because state ${vin + stateId} changed in last 30min TS: ${new Date(curState.ts).toLocaleString()} LC: ${new Date(
             curState.lc
